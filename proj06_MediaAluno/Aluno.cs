9191e6656f58d58e9ae6d4da7b17238fd3ff2be5
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-20 22:03:59 -0300

    Add files via upload

---
+++ proj06_MediaAluno/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proj06_MediaAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double NotaA;
+        public double NotaB;
+        public double NotaC;
+
+
+        public double Soma()
+        {
+            return NotaA + NotaB + NotaC;
+        }
+
+        public void Resultado(double resultado)
+        {
+            if(resultado >= 60)
+            {
+                Console.WriteLine("NOTA FINAL: " + resultado.ToString("F2"));
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("NOTA FINAL: " + resultado.ToString("F2"));
+                Console.WriteLine("REPROVADO");
+                resultado = 60 - resultado;
+                Console.WriteLine("FALTARAM: " + resultado.ToString("F2") + " PONTOS");
+            }
+        }
+    }
+}
---
+++ proj06_MediaAluno/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace proj06_MediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno estudante = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            estudante.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            estudante.NotaA = double.Parse(Console.ReadLine());
+            estudante.NotaB = double.Parse(Console.ReadLine());
+            estudante.NotaC = double.Parse(Console.ReadLine());
+
+            estudante.Soma();
+            double resultado = estudante.Soma();
+
+            estudante.Resultado(resultado);
+            
+        }
+    }
+}
