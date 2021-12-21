788ac814535637326d4850bf9211c5882d56067f
Isabella Ramos <66478873+Isadevs@users.noreply.github.com>
2021-12-21 07:13:22 -0300

    Add files via upload

---
+++ proj07_ConversorDeMoedas/Cotacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proj07_ConversorDeMoedas
+{
+    class Cotacao
+    {
+        public static double ConversorDeMoeda(double cotacao, double dolar)
+        {
+            return cotacao * dolar;
+        }
+
+    }
+}
---
+++ proj07_ConversorDeMoedas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace proj07_ConversorDeMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares você vai comprar: ");
+            double dolar = double.Parse(Console.ReadLine());
+
+            double conversor = Cotacao.ConversorDeMoeda(cotacao, dolar);
+
+            Console.WriteLine("Valor a ser pago em reais: " + conversor.ToString("F2"));
+        }
+    }
+}
