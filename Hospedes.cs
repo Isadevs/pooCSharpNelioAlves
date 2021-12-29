e70498b0c3ad80095ec7eaa7174a7dcd878617aa
Isabella Ramos <Seu Email>
2021-12-29 17:42:15 -0300

    Exercício Pensionato uni_06

---
+++ Hospedes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exPensionato
+{
+    class Hospedes
+    {
+        public string Name { get; set; }
+        public string E_mail { get; set; }
+        public int Room { get; set; }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace exPensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many rooms will be rented? ");
+            int hospedes = int.Parse(Console.ReadLine());
+
+            Hospedes[] vect = new Hospedes[10];
+
+            for(int i = 0; i < hospedes; i++)
+            {
+                int numRoom = i + 1;
+                Console.WriteLine("Rent #" + numRoom);
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string e_mail = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                vect[room] = new Hospedes { Name = name, E_mail = e_mail, Room = room };
+            }
+
+            Console.WriteLine("Busy Rooms: ");
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(vect[i].Room + " :" + vect[i].Name + " ," +  vect[i].E_mail);
+                }
+            }
+        }
+    }
+}
